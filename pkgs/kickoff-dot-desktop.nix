9cdb02e26ac9f769695edb38a96bd2a2af0aa1c1
Yara <git@yara.blue>
2025-12-09 23:38:07 +0100

    fixes wrong hash in kickoff dot desktop nix causing it to build anki instead

--- pkgs/kickoff-dot-desktop.nix
+++ pkgs/kickoff-dot-desktop.nix
@@ -6,16 +6,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "kickoff-dot-desktop";
-  version = "unstable-2023-05-17";
+  version = "unstable-2023-06-17";
 
   src = fetchFromGitHub {
     owner = "j0ru";
     repo = "kickoff-dot-desktop";
     rev = "ba3e8788c7120c95c4ee963abf3904eb0736cb24";
-    hash = "sha256-U8Y/vSLFG8ptEV696TARflhdko7r+9ZmzFyGZTbfeCQ=";
+    hash = "sha256-exMmqOkDKuyAEdda8gG/uF3+tnQzhJnOJK+sEtZbsZc=";
   };
 
-  cargoHash = "sha256-yAmHacaSPbK0w7zN7h0tUvyyekbv3BS6FyPuSzvJofo=";
+  cargoHash = "sha256-4x9kEBnYPhhYej0mNmV7jibw6sH8SxVau9tIkIVTen8=";
 
   meta = {
     description = "Smol program to read in relevant desktop files and print them in a kickoff compatible format";
